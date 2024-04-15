8053f9176c66b9e01d1ed60e8b66da00f966c826
xandale <166452794+xandale@users.noreply.github.com>
2024-04-15 18:55:23 -0300

    Add files via upload

---
+++ repeticoes/exercicios.js
@@ -0,0 +1,12 @@
+// let contador = 0
+// let nota =0
+// let somaDasNotas =0
+// while (nota!=-1){
+// nota=Number(prompt(`Digite suas notas ou -1 para ter a média`))
+//     if (nota != -1){
+//     somaDasNotas=( somaDasNotas + nota)
+//     contador++
+//     }
+// }
+// let media=(somaDasNotas/contador)
+// alert(`a sua média é de ${media.toFixed(2)}`)
