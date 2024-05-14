4cb90660f5322e63c94562ab4e499b5120f7e8ec
xandale <166452794+xandale@users.noreply.github.com>
2024-05-13 21:53:41 -0300

    arrays
    exercicios de arrays

---
+++ array/array2.js
@@ -0,0 +1,21 @@
+let pai, mae
+let filho = []
+let div = document.getElementById('divSaida2')
+function gen(){
+    let num = []
+    for(let i=0;i<26;i++){
+        let a = Math.ceil(Math.random() * 100)
+        num.push(a)
+    }
+    return num
+}
+pai = gen ()
+mae = gen
+()
+for(let i = 0; i < 26; i++){
+    let a = (i%2==0) ? pai[i] : mae[i]
+    filho[i]= a
+}
+div.innerHTML += `mae: ${mae} <br>`
+div.innerHTML += `pai: ${pai} <br>`
+div.innerHTML += `filho: ${filho} <br>`
---
+++ array/array3.js
@@ -0,0 +1,8 @@
+let numeros=[]
+let min = 12
+let max = 35
+let saida = document.getElementById('divSaida2')
+for(i=0;i<30;i++){
+    let n = Math.floor(Math.random() * (max - min + 1) ) + min;
+    numeros.push(n)}
+    console.log(numeros)
