804decce37473601e69f343785d45bc1e092372a
xandale <166452794+xandale@users.noreply.github.com>
2024-06-12 22:04:13 -0300

    aula 39
    Aula 39  - 12/06 Pseudo-classes CSS e classList

---
+++ Aula 39/1.js
@@ -0,0 +1,8 @@
+function clicar(){
+    // importar o elemento
+    const elemento = document.querySelector("#conteudo")
+
+    //Manipular a classe
+    // elemento.classList.add("oculto")
+    elemento.classList.toggle("oculto")
+}
